22b7cecea250fd860391d1b13af498b5e06c7059
skocur <skocur10@gmail.com>
2019-05-29 22:45:22 +0200

    Added empty activities that will be responsible for encryption and decryption

---
+++ src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/DecryptionActivity.kt
@@ -0,0 +1,12 @@
+package com.skocur.imagecipher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DecryptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_decryption)
+    }
+}
--- src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/MainActivity.kt
+++ src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.skocur.imagecipher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainCardEncryption.setOnClickListener {
+            val intent = Intent(this, EncryptionActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        mainCardDecryption.setOnClickListener {
+            val intent = Intent(this, DecryptionActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
