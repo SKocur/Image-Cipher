61c010c8e5bb6a4d89217613d06a92973172604e
skocur <skocur10@gmail.com>
2019-06-07 16:09:26 +0200

    Prepared layout for implementing decryption

--- src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/EncryptionActivity.kt
+++ src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/EncryptionActivity.kt
@@ -2,11 +2,23 @@ package com.skocur.imagecipher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_encryption.*
 
 class EncryptionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encryption)
+
+        buttonEncrypt.setOnClickListener {
+            val text : String = textToEncrypt.text.toString()
+            var option = 3 // Default encryption is set to Low Level Bit Encryption
+
+            if (radioSingleColorEncryption.isChecked) {
+                option = 1
+            } else if (radioMultiColorEncryption.isChecked) {
+                option = 2
+            }
+        }
     }
 }
