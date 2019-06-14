bf05378e1d05a75c56429df16d2c203b783561d5
skocur <skocur10@gmail.com>
2019-06-14 15:18:28 +0200

    Added loading image from smartphone's storage

--- src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/EncryptionActivity.kt
+++ src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/EncryptionActivity.kt
@@ -1,14 +1,29 @@
 package com.skocur.imagecipher
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import kotlinx.android.synthetic.main.activity_encryption.*
 
 class EncryptionActivity : AppCompatActivity() {
 
+    private val REQUEST_PHOTO_GALLERY = 0
+    private lateinit var bitmap : Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encryption)
+
+        buttonEncLoadImage.setOnClickListener {
+            // TODO: Add - asking user for permission to access smartphone's storage
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, REQUEST_PHOTO_GALLERY)
+        }
 
         buttonEncrypt.setOnClickListener {
             val text : String = textToEncrypt.text.toString()
@@ -18,6 +33,19 @@ class EncryptionActivity : AppCompatActivity() {
                 option = 1
             } else if (radioMultiColorEncryption.isChecked) {
                 option = 2
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_PHOTO_GALLERY) {
+            if (data != null) {
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, data.data)
+
+                imageFromGalleryEnc.setImageBitmap(bitmap)
+                imageFromGalleryEnc.visibility = View.VISIBLE
             }
         }
     }
