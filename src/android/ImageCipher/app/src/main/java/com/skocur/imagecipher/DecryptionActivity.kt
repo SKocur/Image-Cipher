95d8a54c176220c580813f0dce5071e77976754d
skocur <skocur10@gmail.com>
2019-06-16 16:36:59 +0200

    Added displaying image in preview DecryptionActivity

--- src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/DecryptionActivity.kt
+++ src/android/ImageCipher/app/src/main/java/com/skocur/imagecipher/DecryptionActivity.kt
@@ -1,12 +1,44 @@
 package com.skocur.imagecipher
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import kotlinx.android.synthetic.main.activity_decryption.*
 
 class DecryptionActivity : AppCompatActivity() {
+
+    private val REQUEST_PHOTO_GALLERY = 0
+    private lateinit var bitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decryption)
+
+        buttonLoadImageDec.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, REQUEST_PHOTO_GALLERY)
+        }
+
+        buttonDecrypt.setOnClickListener {
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_PHOTO_GALLERY) {
+            if (data != null) {
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, data.data)
+
+                imageFromGalleryDec.setImageBitmap(bitmap)
+                imageFromGalleryDec.visibility = View.VISIBLE
+            }
+        }
     }
 }
