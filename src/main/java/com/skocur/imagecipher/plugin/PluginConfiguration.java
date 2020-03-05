5805259a2c783b4435c46138da021b2fa982df21
skocur <skocur10@gmail.com>
2020-03-05 16:19:18 +0100

    Fixed code of plugin loader

--- src/main/java/com/skocur/imagecipher/plugin/PluginConfiguration.java
+++ src/main/java/com/skocur/imagecipher/plugin/PluginConfiguration.java
@@ -1,64 +1,70 @@
 package com.skocur.imagecipher.plugin;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.imagecipher.icsdk.PluginInstance;
 
 import java.util.List;
 
 public class PluginConfiguration {
 
-    private String pluginVersion;
-    private String pluginName;
-    private List<PluginAuthor> pluginAuthors;
-    private String pluginDescription;
-    private String mainClassPath;
+  private String pluginVersion;
+  private String pluginName;
+  private List<PluginAuthor> pluginAuthors;
+  private String pluginDescription;
+  private String mainClassPath;
 
-    private PluginInstance pluginInstance;
+  private PluginInstance pluginInstance;
 
-    public String getPluginVersion() {
-        return pluginVersion;
-    }
+  public String getPluginVersion() {
+    return pluginVersion;
+  }
 
-    public void setPluginVersion(String pluginVersion) {
-        this.pluginVersion = pluginVersion;
-    }
+  @JsonProperty("pluginVersion")
+  public void setPluginVersion(String pluginVersion) {
+    this.pluginVersion = pluginVersion;
+  }
 
-    public String getPluginName() {
-        return pluginName;
-    }
+  public String getPluginName() {
+    return pluginName;
+  }
 
-    public void setPluginName(String pluginName) {
-        this.pluginName = pluginName;
-    }
+  @JsonProperty("pluginName")
+  public void setPluginName(String pluginName) {
+    this.pluginName = pluginName;
+  }
 
-    public List<PluginAuthor> getPluginAuthors() {
-        return pluginAuthors;
-    }
+  public List<PluginAuthor> getPluginAuthors() {
+    return pluginAuthors;
+  }
 
-    public void setPluginAuthors(List<PluginAuthor> pluginAuthors) {
-        this.pluginAuthors = pluginAuthors;
-    }
+  @JsonProperty("pluginAuthors")
+  public void setPluginAuthors(List<PluginAuthor> pluginAuthors) {
+    this.pluginAuthors = pluginAuthors;
+  }
 
-    public String getMainClassPath() {
-        return mainClassPath;
-    }
+  public String getMainClassPath() {
+    return mainClassPath;
+  }
 
-    public void setMainClassPath(String mainClassPath) {
-        this.mainClassPath = mainClassPath;
-    }
+  @JsonProperty("mainClassPath")
+  public void setMainClassPath(String mainClassPath) {
+    this.mainClassPath = mainClassPath;
+  }
 
-    public String getPluginDescription() {
-        return pluginDescription;
-    }
+  public String getPluginDescription() {
+    return pluginDescription;
+  }
 
-    public void setPluginDescription(String pluginDescription) {
-        this.pluginDescription = pluginDescription;
-    }
+  @JsonProperty("pluginDescription")
+  public void setPluginDescription(String pluginDescription) {
+    this.pluginDescription = pluginDescription;
+  }
 
-    public PluginInstance getPluginInstance() {
-        return pluginInstance;
-    }
+  public PluginInstance getPluginInstance() {
+    return pluginInstance;
+  }
 
-    public void setPluginInstance(PluginInstance pluginInstance) {
-        this.pluginInstance = pluginInstance;
-    }
+  public void setPluginInstance(PluginInstance pluginInstance) {
+    this.pluginInstance = pluginInstance;
+  }
 }
