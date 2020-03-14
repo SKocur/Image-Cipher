26078cdf09aa69a363ce36302991e0e9b5c76367
skocur <skocur10@gmail.com>
2020-03-14 20:42:40 +0100

    Refactored plugin loader

--- src/main/java/com/skocur/imagecipher/plugin/PluginLoader.java
+++ src/main/java/com/skocur/imagecipher/plugin/PluginLoader.java
@@ -17,10 +17,12 @@ import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class PluginLoader {
 
-  public static void loadAllPlugins() {
+  static void loadAllPlugins() {
     File[] jars = listJars();
 
     if (jars == null) {
@@ -29,10 +31,10 @@ public class PluginLoader {
 
     List<URI> jarsUrls = listUris(jars);
 
-    jarsUrls.forEach(PluginLoader::loadPluginFromUri);
+    jarsUrls.parallelStream().forEach(PluginLoader::loadPluginFromUri);
   }
 
-  private static void loadPluginFromUri(URI uri) {
+  private static void loadPluginFromUri(@NotNull URI uri) {
     try {
       JarFile jarFile = new JarFile(uri.getPath());
 
@@ -45,43 +47,82 @@ public class PluginLoader {
         return;
       }
 
-      BufferedReader in = new BufferedReader(
-          new InputStreamReader(jarFile.getInputStream(jarEntry)));
-      StringBuilder yamlContentBuilder = new StringBuilder();
-      String line;
-      while ((line = in.readLine()) != null) {
-        yamlContentBuilder.append(line).append(System.lineSeparator());
+      PluginConfiguration pluginConfiguration = getYamlMapper()
+          .readValue(getYamlContent(jarFile.getInputStream(jarEntry)), PluginConfiguration.class);
+
+      ClassLoader loader = getPluginLoader(uri);
+
+      if (loader == null) {
+        System.err.println(
+            String.format("Class loader is null, because of malformed uri: %s", uri.toString()));
+        return;
       }
 
-      PluginConfiguration pluginConfiguration = getYamlMapper()
-          .readValue(yamlContentBuilder.toString(), PluginConfiguration.class);
-
-      ClassLoader loader = new URLClassLoader(
-          new URL[]{uri.toURL()},
-          PluginManager.class.getClassLoader()
-      );
-
-      IcPlugin plugin = (IcPlugin) loader.loadClass(pluginConfiguration.getMainClassPath())
-          .newInstance();
-      PluginInstance instance = plugin.onPluginLoaded();
-      pluginConfiguration.setPluginInstance(instance);
-
-      PluginManager.getPlugins().add(pluginConfiguration);
+      loadPlugin(loader, pluginConfiguration);
     } catch (MalformedURLException e) {
       e.printStackTrace();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
-    } catch (ClassNotFoundException e) {
+    }
+  }
+
+  private static void loadPlugin(@NotNull ClassLoader loader,
+      @NotNull PluginConfiguration pluginConfiguration) {
+    IcPlugin plugin;
+    try {
+      plugin = (IcPlugin) loader.loadClass(pluginConfiguration.getMainClassPath())
+          .newInstance();
+
+      PluginInstance instance = plugin.onPluginLoaded();
+      pluginConfiguration.setPluginInstance(instance);
+
+      PluginManager.getPlugins().add(pluginConfiguration);
+    } catch (InstantiationException e) {
       e.printStackTrace();
     } catch (IllegalAccessException e) {
       e.printStackTrace();
-    } catch (InstantiationException e) {
+    } catch (ClassNotFoundException e) {
       e.printStackTrace();
     }
   }
 
+  @Nullable
+  private static ClassLoader getPluginLoader(@NotNull URI uri) {
+    try {
+      return new URLClassLoader(
+          new URL[]{uri.toURL()},
+          PluginManager.class.getClassLoader()
+      );
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+
+    return null;
+  }
+
+  @NotNull
+  private static String getYamlContent(@NotNull InputStream inputStream) {
+    BufferedReader in = new BufferedReader(
+        new InputStreamReader(inputStream));
+    StringBuilder yamlContentBuilder = new StringBuilder();
+    String line;
+    try {
+      while ((line = in.readLine()) != null) {
+        yamlContentBuilder.append(line).append(System.lineSeparator());
+      }
+    } catch (IOException e) {
+      System.err.println(
+          String
+              .format("IOException occured while reading contents of yaml file, %s", e.getMessage())
+      );
+    }
+
+    return yamlContentBuilder.toString();
+  }
+
+  @NotNull
   private static ObjectMapper getYamlMapper() {
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -89,12 +130,14 @@ public class PluginLoader {
     return mapper;
   }
 
+  @NotNull
   private static List<URI> listUris(File[] jars) {
     return Arrays.stream(jars)
         .map(File::toURI)
         .collect(Collectors.toList());
   }
 
+  @Nullable
   private static File[] listJars() {
     File dir = new File(PluginManager.PLUGINS_PATH);
     return dir.listFiles((directory, name) -> name.endsWith(".jar"));
