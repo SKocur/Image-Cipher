86f9b22d01c9500f271f7bc800dd1df284b49fe7
skocur <skocur10@gmail.com>
2020-03-26 09:38:45 +0100

    Removed frequent logging

--- src/main/java/com/skocur/imagecipher/tools/ColorParser.java
+++ src/main/java/com/skocur/imagecipher/tools/ColorParser.java
@@ -1,16 +1,11 @@
 package com.skocur.imagecipher.tools;
 
 import javafx.scene.paint.Color;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class ColorParser {
 
-  private static final Logger logger = LogManager.getLogger();
-
   public static int getColor(@NotNull Color color) {
-    logger.debug("Parsing color: " + color.toString());
     int R = (int) Math.round(255 * color.getRed());
     int G = (int) Math.round(255 * color.getGreen());
     int B = (int) Math.round(255 * color.getBlue());
